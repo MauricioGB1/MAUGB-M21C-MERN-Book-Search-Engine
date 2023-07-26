3bf114f6e7bd764786946eb9967134895e971c42
Mauricio Gomez <gomezb_mauricio@hotmail.com>
2023-07-25 19:20:09 -0500

    updates to main structure

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,39 +1,52 @@
-import {
-  Jumbotron,
-  Container,
-  CardColumns,
-  Card,
-  Button,
-} from "react-bootstrap";
-
-import { removeBookId } from "../utils/localStorage";
-import { useQuery, useMutation } from "@apollo/client";
-import { QUERY_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations";
+// React setup
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+// import the 'auth' setup
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+// need these to refactor for GraphQL API
+import { useQuery, useMutation } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 
 const SavedBooks = () => {
-  const { data, loading } = useQuery(QUERY_ME);
-  var userData = data?.me || {};
-  const [removeBook] = useMutation(REMOVE_BOOK);
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  
+  const userData = data?.me || {};
 
-  async function handleDeleteBook(bookId) {
+  // function to delete book from database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    // new code
     try {
-      await removeBook({
-        variables: {bookId: bookId}
-      });
+    const response = await removeBook({ variables: { bookId } });
+        console.log('Deleted record: ', response);
+        if (error) {
+          console.log(error);
+        }
+      // also remove from Localstorage
       removeBookId(bookId);
     } catch (err) {
-      console.error(err);
+      // display any caught errors here
+        console.error(err);
     }
-  }
+  };
 
+  // if data isn't here yet, say so
+  // new code
   if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <Jumbotron fluid className="text-light bg-dark">
+      <Jumbotron fluid className='text-light bg-dark'>
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -41,29 +54,19 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? "book" : "books"
-              }:`
-            : "You have no saved books!"}
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
         </h2>
         <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
-                  <Card.Link href={book.link} target="_blank" rel="noreferrer">
-                    <Card.Img
-                      src={book.image}
-                      alt={`The cover for ${book.title}`}
-                      variant="top"
-                    />
-                  </Card.Link>
-                ) : null}
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
+                  <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,65 +1,76 @@
-mport { gql } from '@apollo/client';
+// initial setup for Apollo Client for React
+import { gql } from '@apollo/client';
 
-export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-            }
-        }
-    }
-`;
-
+// boilerplate for User login controls
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-                }
-            }
-        }
-    `;
-
-    export const SAVE_BOOK = gql`
-    mutation saveBook($bookId: String!, $description: String!, $title: String! $authors: [String], $image: String, $link: String) {
-        saveBook(bookId: $bookId, description: $description, title: $title authors: $authors, image: $image, link: $link) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                title
-                description
-                authors
-                image
-                link
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
-export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: String!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                title
-                description
-                authors
-                image
-                link
-            }
+// modify for refactored code
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
         }
+      }
     }
+  }
+`;
+
+// new code for refactor
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: InputBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// new code for refactor
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
 `;
